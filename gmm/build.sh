cfeab555a7436bfd492a56ed84f08d037a27b40c
DASKR <167687177+DASKR515@users.noreply.github.com>
2026-07-13 22:49:27 +0300

    my bad

--- gmm/build.sh
+++ gmm/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-GOOS=windows GOARCH=amd64 go build -o gmm.exe
-GOOS=linux GOARCH=amd64 go build -o gmm
-GOOS=linux GOARCH=arm64 go build -o gmm-arm64
+GOOS=windows GOARCH=amd64 go build -o gmm.exe mainAll.go
+GOOS=linux GOARCH=amd64 go build -o gmm mainAll.go
+GOOS=linux GOARCH=arm64 go build -o gmm-arm64 mainAll.go
