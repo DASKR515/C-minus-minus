ca5d43281fedd1ab9adacf6b3fd17ae5caab1192
DASKR <167687177+DASKR515@users.noreply.github.com>
2026-09-26 16:14:45 +0300

    Create ret.h for return handling macros
    Add header file for return macros in CMM.

---
+++ gmm/libgmm/ret.h
@@ -0,0 +1,28 @@
+#ifndef CMM_RET_H
+#define CMM_RET_H
+
+#define fnc(name) \
+    name (W_ ret_addr)
+
+#define fnc_a(name, ...) \
+    name (W_ ret_addr, __VA_ARGS__)
+
+#define ret_v() \
+    jump ret_addr ()
+
+#define ret(val) \
+    jump ret_addr (val)
+
+#define ret2(v1, v2) \
+    jump ret_addr (v1, v2)
+
+#define ret3(v1, v2, v3) \
+    jump ret_addr (v1, v2, v3)
+
+#define mall_v(fn, cont) \
+    jump fn (cont)
+
+#define mall(fn, cont, ...) \
+    jump fn (cont, __VA_ARGS__)
+
+#endif
