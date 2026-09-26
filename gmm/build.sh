3e26fbf3e762861af982d8004a251699036c3444
DASKR <167687177+DASKR515@users.noreply.github.com>
2026-09-26 22:20:59 +0300

    Update build script to use main.go instead of mainAll.go

--- gmm/build.sh
+++ gmm/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-GOOS=windows GOARCH=amd64 go build -o gmm.exe mainAll.go
-GOOS=linux GOARCH=amd64 go build -o gmm mainAll.go
-GOOS=linux GOARCH=arm64 go build -o gmm-arm64 mainAll.go
+GOOS=windows GOARCH=amd64 go build -o gmm.exe main.go
+GOOS=linux GOARCH=amd64 go build -o gmm main.go
+GOOS=linux GOARCH=arm64 go build -o gmm-arm64 main.go
